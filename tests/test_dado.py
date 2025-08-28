183c11b7e44bfa5d624e9692275a62d8da7a9cd5
Gustavo <gustavo.gonzalez8190@gmail.com>
2025-08-28 15:44:44 -0400

    Patch

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,6 +1,14 @@
+import os
+import sys
 from src.juego.dado import Dado
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 def test_lanzar():
     dado = Dado()
     dado.lanzar()
     assert dado.cara in [1, 2, 3, 4, 5, 6]
+
+"""def test_pinta():
+    dado = Dado()
+    dado.lanzar()
+    dado.pintar()
+    assert dado.pinta in ["As", "Tonto", "Tren", "Cuadra", "Quina", "Sexto"]"""
