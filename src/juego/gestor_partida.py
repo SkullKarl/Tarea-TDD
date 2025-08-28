8aac28c71f186464a44fbd4c9f856333a97b06a6
Gustavo <gustavo.gonzalez8190@gmail.com>
2025-08-28 11:04:54 -0400

    Estructura y requiremets.txt

---
+++ src/juego/gestor_partida.py
@@ -0,0 +1,8 @@
+class GestorPartida:
+
+    def __init__(self, arbitro, jugadores):
+        self.arbitro = arbitro
+        self.jugadores = jugadores
+
+    def IniciarPartida(self):
+        return
