0111697117138dbc97bdfc77ffe79c106d3f67c8
SebaGarcias <s.garcias1963@gmail.com>
2025-08-28 16:14:04 -0400

    GREEN: implementación mínima para obterner denominación de la cara

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -1,9 +1,22 @@
 import random
 
 class Dado:
-
-    def __init__(self):
-        self.cara = 1
+    def __init__(self, cara:int = None):
+        if(cara == None):
+            self.cara = 1
+            return
+        self.cara = cara
 
     def lanzar(self):
         self.cara = random.randint(1, 6)
+    
+    def GetDenominacion(self):
+        denominaciones = {
+            1: "As",
+            2: "Tonto",
+            3: "Tren",
+            4: "Cuadra",
+            5: "Quina",
+            6: "Sexto"
+        }
+        return denominaciones.get(self.cara, "Desconocido")
