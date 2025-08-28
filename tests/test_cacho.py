ac76e7bd0358c821f49990e9474dbfba7d4574ce
SebaGarcias <s.garcias1963@gmail.com>
2025-08-28 16:42:46 -0400

    RED: test para Agitar cacho - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -1,8 +1,20 @@
 from src.juego.cacho import Cacho
 from src.juego.dado import Dado
+import random
 
 def test_init_cacho():
     cacho = Cacho()
     for dado in cacho.dados:
         assert isinstance(dado, Dado)
     assert len(cacho.dados) == 5
+
+def test_agitar_cacho():
+    cacho = Cacho()
+    random.seed(2) # Fijar la semilla para evitar mismos resultados
+    dados = cacho.dados 
+    cacho.agitar()
+    valores_iniciales = [d.GetDenominacion() for d in dados]
+    cacho.agitar()
+    valores_revueltos = [d.GetDenominacion() for d in dados]
+
+    assert valores_iniciales != valores_revueltos
