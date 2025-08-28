a0a04f793df24935074d1ec2d91f7b860b494de9
SebaGarcias <s.garcias1963@gmail.com>
2025-08-28 16:44:43 -0400

    GREEN: implementación mínima para Agitar cacho

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -5,3 +5,8 @@ class Cacho:
     def __init__(self):
         self.dados = [Dado() for _ in range(5)]
         return
+
+    def agitar(self):
+        for dado in self.dados:
+            dado.lanzar()
+        return
