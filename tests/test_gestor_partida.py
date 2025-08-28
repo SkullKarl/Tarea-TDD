1ab1e462be868839eb3686a5d9d31b8ba69c9116
Gustavo <gustavo.gonzalez8190@gmail.com>
2025-08-28 15:38:34 -0400

    GitHub actions try.

---
+++ tests/test_gestor_partida.py
@@ -0,0 +1,18 @@
+from src.juego.gestor_partida import GestorPartida
+
+def test_IniciarPartida():
+    GP = GestorPartida()
+    assert
+
+def test_IniciarRonda():
+    GP = GestorPartida()
+    assert
+
+def test_obligar():
+    GP = GestorPartida()
+    GP.obligar()
+    assert GP ==
+
+def test_FinalizarPartida():
+    GP = GestorPartida()
+    assert
