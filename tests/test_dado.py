dfb654e8fdee991c78b589a5237661b74c0acabf
SebaGarcias <s.garcias1963@gmail.com>
2025-08-28 16:07:35 -0400

    "RED: test para obterner denominación - falla como esperado"

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -4,3 +4,18 @@ def test_lanzar():
     dado = Dado()
     dado.lanzar()
     assert dado.cara in [1, 2, 3, 4, 5, 6]
+
+def test_GetDenominacion():
+    dado_1 = Dado(1)
+    dado_2 = Dado(2)
+    dado_3 = Dado(3)
+    dado_4 = Dado(4)
+    dado_5 = Dado(5)
+    dado_6 = Dado(6)
+
+    assert dado_1.GetDenominacion() == "As" 
+    assert dado_2.GetDenominacion() == "Tonto" 
+    assert dado_3.GetDenominacion() == "Tren" 
+    assert dado_4.GetDenominacion() == "Cuadra" 
+    assert dado_5.GetDenominacion() == "Quina"
+    assert dado_6.GetDenominacion() == "Sexto"
