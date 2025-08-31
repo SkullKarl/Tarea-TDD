499123f97127acaa440a915176170b6e0abdfa3d
SebaGarcias <s.garcias1963@gmail.com>
2025-08-31 16:06:37 -0400

    GREEN: implementación mínima para el constructor del cacho de tener 5 dados inicialmente

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -1,3 +1,9 @@
+from .dado import Dado
+
 class Cacho:
     def __init__(self):
+        self.__dado = [Dado() for _ in range(5)]
         pass
+
+    def GetDados(self):
+        return self.__dado
