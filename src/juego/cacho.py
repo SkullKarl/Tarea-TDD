141dc506f8987ecf362eb69eca652a15bc3e33a2
SebaGarcias <s.garcias1963@gmail.com>
2025-08-31 16:17:13 -0400

    GREEN: implementación mínima para [Mostrar/Ocultar cacho]

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -2,12 +2,21 @@ from .dado import Dado
 
 class Cacho:
     def __init__(self):
+        self.__visible = True
         self.__dado = [Dado() for _ in range(5)]
         pass
 
     def GetDados(self):
+        if (self.__visible == False):
+            return None
         return self.__dado
 
     def agitar(self):
         for dado in self.__dado:
             dado.lanzar()
+
+    def ocultar(self):
+        self.__visible = False
+    
+    def mostrar(self):
+        self.__visible = True
