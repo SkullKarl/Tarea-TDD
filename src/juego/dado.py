20ebd1320294fb7245493740a86f3cf751980cc1
SebaGarcias <s.garcias1963@gmail.com>
2025-08-31 21:46:00 -0400

    PARCHE: Limpiar un residuo de un rebase de un commit de dado anterior, no modifica comportamiento

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -20,12 +20,4 @@ class Dado:
         self.cara = random.randint(1, 6)
     
     def GetDenominacion(self):
-        denominaciones = {
-            1: "As",
-            2: "Tonto",
-            3: "Tren",
-            4: "Cuadra",
-            5: "Quina",
-            6: "Sexto"
-        }
-        return denominaciones.get(self.cara, "Desconocido")
+        return Pintas(self.cara).name 
