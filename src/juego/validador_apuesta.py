d3f5de757b7f6872d7b39335fd30cf85eb0741e2
SebaGarcias <s.garcias1963@gmail.com>
2025-08-31 20:03:03 -0400

    GREEN: implementación mínima para [Si una nueva apuesta es válida (mayor cantidad o pinta superior)]

--- src/juego/validador_apuesta.py
+++ src/juego/validador_apuesta.py
@@ -1,3 +1,30 @@
+from juego.dado import Pintas
+
 class ValidadorApuesta:
     def __init__(self):
         pass
+
+    def esValido(self, apuesta_anterior, apuesta_nueva):
+        cantidad_anterior, tipo_anterior = apuesta_anterior
+        cantidad_nueva, tipo_nueva = apuesta_nueva
+
+        tipo_anterior_int = Pintas[tipo_anterior].value
+        tipo_nueva_int = Pintas[tipo_nueva].value
+
+        if (cantidad_anterior > cantidad_nueva):
+            return False
+
+        if (tipo_anterior_int > tipo_nueva_int):
+            return False
+        
+        if (cantidad_anterior == cantidad_nueva and tipo_anterior == tipo_nueva):
+            return False
+
+        if(tipo_anterior_int < tipo_nueva_int and cantidad_anterior == cantidad_nueva):
+            return True
+        
+        if(tipo_anterior_int == tipo_nueva_int and cantidad_anterior < cantidad_nueva):
+            return True
+        
+        return False
+        
