8b1920b73f1f277d0f14de59676e7edde561c637
gustavo <gustavo.gonzalez8190@gmail.com>
2025-09-01 17:08:30 -0400

    RED: test para [Jugar una ronda (con la opcion de obligar)] - falla como esperado

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -10,3 +10,20 @@ def test_IniciarPartida():
     X = GP.IniciarPartida()
     assert isinstance(X, int)
     assert X in range(1, len(j))
+
+def test_JugarRonda():
+    j = ['1', '2', '3']
+    J = [Cacho(), Cacho(), Cacho()]
+    A = ArbitroRonda(j, J)
+    GP = GestorPartida(A, J)
+    X = GP.JugarRonda()
+    assert J != X
+
+def test_obligar():
+    j = ['1', '2', '3']
+    J = [Cacho(), Cacho(), Cacho()]
+    A = ArbitroRonda(j, J)
+    GP = GestorPartida(A, J)
+    jugador_con_un_dado = J[1]
+    X = GP.obligar(jugador_con_un_dado)
+    assert X == True
