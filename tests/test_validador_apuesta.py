ea40374ca5e9da40c3072c759123dafdb1d513f3
SebaGarcias <s.garcias1963@gmail.com>
2025-08-31 20:50:28 -0400

    RED: test para [Las reglas especiales de los Ases] - falla como esperado

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -26,3 +26,46 @@ def test_apuesta_valida():
     apuesta_anterior = (3, "Tren")
     apuesta_nueva = (3, "Tonto")
     assert False == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+def test_apuesta_caso_ases():
+    validador = ValidadorApuesta()
+
+    # --------- A AS|ES ---------
+    #
+    
+    # MITAD REDONDEADA PARA ARRIBA [IMPRAR]
+    apuesta_anterior = (7, "Tren")
+    apuesta_nueva = (4, "As")
+    assert True == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+    apuesta_anterior = (7, "Tren")
+    apuesta_nueva = (3, "As")
+    assert False == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+    # MITAD MAS UNO [PAR]
+    apuesta_anterior = (8, "Cuadra")
+    apuesta_nueva = (5, "As")
+    assert True == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+    apuesta_anterior = (8, "Cuadra")
+    apuesta_nueva = (4, "As")
+    assert False == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+    # --------- DE ASES ---------
+    #
+    apuesta_anterior = (2, "As")
+    apuesta_nueva = (5, "Quinta")
+    assert True == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+    apuesta_anterior = (2, "As")
+    apuesta_nueva = (4, "Quinta")
+    assert False == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+    # --------- DE ASES HACIA ASES ---------
+    apuesta_anterior = (2, "As")
+    apuesta_nueva = (5, "As")
+    assert False == validador.esValido(apuesta_anterior, apuesta_nueva)
+
+    apuesta_anterior = (8, "As")
+    apuesta_nueva = (5, "As")
+    assert False == validador.esValido(apuesta_anterior, apuesta_nueva)
